45ba79924a3a974c6907e30e50ff35176ab45e37
Cody Fincher <cody.fincher@gmail.com>
2021-01-21 18:08:27 -0600

    update default login endpoint

--- drf_inertia/config.py
+++ drf_inertia/config.py
@@ -10,20 +10,23 @@ TEMPLATE = getattr(settings, 'INERTIA_HTML_TEMPLATE', 'index.html')
 # the django template var the inertia json should be set to
 TEMPLATE_VAR = getattr(settings, 'INERTIA_TEMPLATE_VAR', 'inertia_json')
 
-SHARED_DATA_SERIALIZER = getattr(settings, 'INERTIA_SHARED_SERIALIZER', 'drf_inertia.serializers.DefaultSharedSerializer')
+SHARED_DATA_SERIALIZER = getattr(
+    settings, 'INERTIA_SHARED_SERIALIZER', 'drf_inertia.serializers.DefaultSharedSerializer')
 
 # The exception handler for inertia requests
 # ensures that exceptions are returned in interia
 # format
-EXCEPTION_HANDLER = getattr(settings, 'INERTIA_EXCEPTION_HANDLER', 'drf_inertia.exceptions.DefaultExceptionHandler')
+EXCEPTION_HANDLER = getattr(settings, 'INERTIA_EXCEPTION_HANDLER',
+                            'drf_inertia.exceptions.DefaultExceptionHandler')
 
 # The auth redirect is used in the default exception handler
 # to determine where to go when 401 or 403 errors are raised
-AUTH_REDIRECT = getattr(settings, 'INERTIA_AUTH_REDIRECT', '/login')
+AUTH_REDIRECT = getattr(settings, 'INERTIA_AUTH_REDIRECT', '/accounts/login')
 
 # if AUTH_REDIRECT_URL_NAME is specified use django.urls.reverse
 # to get the AUTH_REDIRECT instead
-AUTH_REDIRECT_URL_NAME = getattr(settings, 'INERTIA_AUTH_REDIRECT_URL_NAME', None)
+AUTH_REDIRECT_URL_NAME = getattr(
+    settings, 'INERTIA_AUTH_REDIRECT_URL_NAME', None)
 
 # DEBUG
 DEBUG = settings.DEBUG
